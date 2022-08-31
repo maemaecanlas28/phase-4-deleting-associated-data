d7dfb7c55bcc4b02716dfbe185f00af087909364
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-31 10:27:49 -0700

    DONE with the assignment

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,5 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+
+  # as soon as .destroy is called on an instance of a DogHouse, all the reviews assosciated with that instance will be destroyed.
+  has_many :reviews, dependent: :destroy
 end
